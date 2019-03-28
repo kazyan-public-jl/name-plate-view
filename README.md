9354819c0aa86ad15716e69291fc2b2cf8edab9d
kazyan-public-jl <kazyan.public.jl@gmail.com>
2019-03-29 03:46:50 +0900

    テキストフォームに入力されたCSV情報から、名札のレイアウトを作成するページ

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# workbase構築
+# name-plate-view
 
 ## 概要
 
-Vue.js+Webpack+Babelな環境があり、ローカルのURLながら画面が表示される状態を用意できる
+指定したフォルダにある画像を順番に読み込み、
+名札に使えるレイアウトに整えて配置し、
+そのまま印刷できる形で出力する。
 
 ```
 # リポジトリ取得
-git clone https://github.com/kazyan-public-jl/workbase.git
+git clone https://github.com/kazyan-public-jl/name-plate-view.git
 
 # リポジトリに移動
-cd workbase
+cd name-plate-view
 
 # 必要モジュールのインストール
 yarn
@@ -27,131 +29,14 @@ localhost:8080
 127.0.0.1:8080
 ```
 
-以上。
+一旦、移植しやすいようにベタなHTML/CSS/JSのファイル構成で作成した。
 
-## 環境コピーの仕方
-
-### 履歴ごとコピーしたい場合の手順
-
-1. workbaseリポジトリを別名でcloneする
-```
-git clone https://github.com/kazyan-public-jl/workbase.git hogehoge
-```
-2. cloneしたフォルダに入る
-```
-cd hogehoge/
-```
-3. originのリモートURLを自分の意図したURLに変更する
-```
-git remote set-url origin https://github.com/some-other-username/hogehoge.git
-```
-4. 新しいリモートURLに設定したリポジトリに対して `--mirror` オプションをつけて `git push` する
-```
-git push --mirror origin
-```
-
-## 目的
-
-- フロントエンドとして作業する際、作業環境の構築から行なっていると時間が足りないことが多いため、あらかじめ雛形を用意しておくことで、開発速度をあげたい。
-- ひとまず画面が表示できるところまでの環境が揃えば、あとはどうにかできる。
-
-## Goal
-
-以下の環境が構築できること。
-
-- ES6環境
-    - 最新のJS。古いブラウザだと動かないことがある
-- ESLint
-    - 文法チェック
-- Prittier
-    - コードフォーマッターと呼ばれるコードを自動であるべき形式に直してくれるツール
-- Babel
-    - 古いブラウザでも表示できるようにES6を変換してくれる
-- Webpack
-    - 各リソースやアセットなどを一つのファイルにバンドル（まとめる）&コンパイルしてくれる便利ツール
-- Vue.js
-    - javascript上で動くProgressive Javascript Frameworkでjsフレームワークの一種。最近流行り。
-- Vuex
-    - ページやオブジェクトの「状態」を管理する仕組み。一つの状態によって複数の部品が作用しあったりするデータを管理させる。
-- Vue-Router
-    - Vueで作られたリソースに対してURLでアクセスできるようルーティング機能を提供するVue.js公式のエコシステム。
+name-plate-view/simpleHTML/index.html
+name-plate-view/simpleHTML/NamePlateView.css
+name-plate-view/simpleHTML/NamePlateView.js
 
 ## 参考
 
-- [モダンフロントエンド開発環境からVue.js環境までひと通り作ってみた](https://qiita.com/isihigameKoudai/items/520c1cb6540e0641a00c)
-- [既存のリポジトリから新規リポジトリを作成したい - teratail.com](https://teratail.com/questions/31714)
-
-
-## 環境構築
-
-概ね参考ページ通りに進めた。
-
-1. 作業ディレクトリ作成
-1. gitとyarn(npmでもOK)の初期化
-1. gitignoreとeditorconfigの追加
-1. ES6とESLintとPrettier
-1. babelの設定
-1. webpackの設定
-1. パッケージの準備
-1. 起動できたらしい。
-1. ディレクトリの準備
-1. 各ファイルの準備
-    - /dest/index.html
-    - /src/App.vue
-    - /src/index.js
-    - /store/index.js
-    - /route/index.js
-    - /src/pages/top.vue
-    - /src/pages/about.vue
-
-
-最後に `yarn start` で webpack-dev-server を起動すれば、URLからwebページが表示される。
-```
-$ yarn start
-
-yarn run v1.12.3
-$ webpack-dev-server --config ./webpack.config.js --env.production --host 0.0.0.0
-ℹ ｢wds｣: Project is running at http://0.0.0.0:8080/
-ℹ ｢wds｣: webpack output is served from /
-ℹ ｢wds｣: Content not from webpack is served from dest
-ℹ ｢wdm｣: Hash: 215552f85a5521487338
-Version: webpack 4.28.4
-Time: 1786ms
-Built at: 2019-01-12 18:01:03
-    Asset      Size  Chunks             Chunk Names
-bundle.js  1.25 MiB    main  [emitted]  main
-Entrypoint main = bundle.js
-[0] multi (webpack)-dev-server/client?http://0.0.0.0:8080 babel-polyfill ./src/index.js 52 bytes {main} [built]
-(略)
-[./src/index.js] 313 bytes {main} [built]
-    + 368 hidden modules
-ℹ ｢wdm｣: Compiled successfully.
-```
-
-MacでChromeから見ようとすると、`localhost:8080`ではなく`127.0.0.1:8080`であることに注意。
-```
-#win
-localhost:8080
-
-#mac
-127.0.0.1:8080
-```
-
-## 記事に記載されてなかったので対応したこと
-
-1. [vue-loaderが新しすぎるとエラーが出る](https://qiita.com/tosite0345/items/2b153049805190fc5d15)らしいので、一つ手前の@14にした
-```
-yarn add vue-loader@14
-```
-
-2. vue-template-compilerがインストールされてなかったのでエラーがでた
-```
-ERROR in ./src/App.vue
-Module build failed (from ./node_modules/vue-loader/index.js):
-Error: Cannot find module 'vue-template-compiler'
-```
-不足してるモジュールをインストールして解決した
-```
-$ yarn add vue-template-compiler
-```
-
+- [【決定版】印刷用CSSはこれでいこう！](https://m-pixellabo.com/blog/%E3%80%90%E6%B1%BA%E5%AE%9A%E7%89%88%E3%80%91%E5%8D%B0%E5%88%B7%E7%94%A8css%E3%81%AF%E3%81%93%E3%82%8C%E3%81%A7%E3%81%84%E3%81%93%E3%81%86%EF%BC%81/)
+- [Document.createElement - Web API | MDN](https://goo.gl/ce0bKK)
+- [rotate() - CSS: カスケーディングスタイルシート | MDN](https://developer.mozilla.org/ja/docs/Web/CSS/transform-function/rotate)
---
+++ simpleHtml/NamePlateView.js
@@ -0,0 +1,168 @@
+/**
+ * 名札レイアウトを表示する
+ */
+function showNamePlateView(event) {
+    const userNamePlateData = generateNamePlateData();
+    const namePlateView = generateNamePlateListLayout(userNamePlateData);
+
+    const namePlateViewElement = document.getElementById('RenderLayoutsArea');
+    namePlateViewElement.innerHTML = "";
+    namePlateViewElement.appendChild(namePlateView);
+}
+/**
+ * 入力されたテキストから名札情報を作成
+ * @return JSON 名札情報
+ */
+function generateNamePlateData() {
+    console.log("onClick generateNamePlateData!");
+    const userList = [];
+    // 参加者一覧を取得
+    const userListElement = document.getElementById('UserList');
+    console.log("userListElement:", typeof userListElement, userListElement);
+    if (userListElement == "") {
+        alert("#UserList エレメントがありません");
+        return false;
+    }
+
+    const userListText = userListElement.value;
+    console.log("userListText:", typeof userListText, userListText);
+    if (userListText == "") {
+        alert("参加者名と所属を入力してください");
+        return false;
+    }
+
+    if (userListText != null) {
+        // 各行データを配列で取得
+        const userListRows = userListText.split(/\r\n|\r|\n/);
+        // userListを作成
+        for (let index = 0; index < userListRows.length; index++) {
+            let rowData = userListRows[index];
+                rowData = rowData.split(',');
+            console.log("rowData:", rowData);
+            // 名前の取得
+            let userName = "";
+            if(rowData[0] == null) {
+                alert("名前を入力してください:"+(index+1)+"行目");
+                return false;
+            } else {
+                userName = rowData[0].trim();
+            }
+
+            // 所属の取得
+            let userRole = "";
+            if(rowData[1] == null) {
+                alert("所属を入力してください:"+(index+1)+"行目");
+                return false;
+            } else {
+                userRole = rowData[1].trim();
+            }
+
+            // 画像パスの登録
+            let userImage = "";
+            if(rowData[2] == null) {
+                alert("Slack画像URLを入力してください:"+(index+1)+"行目");
+                return false;
+            } else {
+                userImage = rowData[2].trim();
+            }
+
+
+            // ユーザーデータの登録
+            userList.push({ 
+                name: userName,
+                role: userRole,
+                image: userImage
+            });
+        }
+    }
+    console.log("userList:", userList);
+    // return userList;
+
+    if (userList.length > 0) {
+        const renderAreaElement = document.getElementsByClassName('renderArea')[0];
+        if (renderAreaElement == null) {
+            return;
+        }
+        // 確認用出力
+        let userListOutputText = JSON.stringify(userList);
+        userListOutputText = userListOutputText.replace(/},{/,'},<br>{');
+        renderAreaElement.innerHTML = userListOutputText;
+
+        return userList;
+    }
+};
+/**
+ * 名札リストのレイアウトを出力する
+ * @param userList Array 名札データ
+ */
+function generateNamePlateListLayout(userList = []) {
+    console.log("generateNamePlateLayout userList < ", userList);
+    if (userList.length <= 0) {
+        return;
+    }
+    const namePlateView = document.createElement('div', {
+        id: 'NamePlateView'
+    });
+    console.log("namePlateView:", namePlateView);
+    for (let index = 0; index < userList.length; index++) {
+        const userData = userList[index];
+        const namePlateElement = generateNamePlateLayout(userList[index], index);
+        if (namePlateElement != null) {
+            // 片面分を設定
+            const namePlate = document.createElement('div');
+            namePlate.className = "name-plate-view";
+            namePlate.appendChild(namePlateElement.cloneNode(true));
+            // 裏面分を設定
+            const namePlateReverse = document.createElement('div');
+            namePlateReverse.className = "name-plate-view reverse";
+            namePlateReverse.appendChild(namePlateElement.cloneNode(true));
+
+            // 余白分を設定
+            const namePlateBlank = document.createElement('div');
+            namePlateBlank.className = "name-plate-view blank";
+            
+            // 両面分を登録
+            const namePlateWrapper = document.createElement('div');
+            namePlateWrapper.className = "name-plate-view-wrapper user"+index;
+            namePlateWrapper.appendChild(namePlateBlank);
+            namePlateWrapper.appendChild(namePlateReverse);
+            namePlateWrapper.appendChild(namePlate);
+            namePlateWrapper.appendChild(namePlateBlank.cloneNode(true));
+
+            // 一人分のレイアウトをリストに登録
+            namePlateView.appendChild(namePlateWrapper);
+        }
+    }
+    console.log("namePlateView=>", namePlateView);
+    return namePlateView;
+}
+
+/**
+ * １つ分の名札エレメントを作成する
+ * @param userData Object ユーザ一人分のJSONデータ
+ * @param index int レイアウト番号
+ */
+function generateNamePlateLayout(userData = {}, index = 0) {
+    // 画像エレメントを作成する
+    const userImageElement = document.createElement('img');
+    userImageElement.setAttribute('src', userData.image);
+    userImageElement.className = "user-image";
+    // 名前エレメントを作成する
+    const userNameElement = document.createElement('div');
+    userNameElement.textContent = userData.name;
+    userNameElement.className = "user-name";
+    // 所属エレメントを作成する
+    const userRoleElement = document.createElement('div');
+    userRoleElement.textContent = userData.role;
+    userRoleElement.className = "user-role";
+
+    // 名札エレメントを作成する
+    const namePlateElement = document.createElement('div');
+    namePlateElement.className = 'namePlate'+index;
+    namePlateElement.appendChild(userImageElement);
+    namePlateElement.appendChild(userNameElement);
+    namePlateElement.appendChild(userRoleElement);
+
+    console.log("generateNamePlateLayout:", index, userData, namePlateElement);
+    return namePlateElement;
+}
